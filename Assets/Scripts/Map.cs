a6f0b10291fa085aa375661fcbee24f2298d0ea6
pham-t-alex <pham.t.alex@gmail.com>
2025-03-12 00:15:15 -0700

    wave spawning

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Map", menuName = "Scriptable Objects/Map")]
+public class Map : ScriptableObject
+{
+    public List<Vector2> frontLinePath = new List<Vector2>();
+    public List<Vector2> backLinePath = new List<Vector2>();
+    public List<MapEndRegion> frontLineEnds = new List<MapEndRegion>();
+    public List<MapEndRegion> backLineEnds = new List<MapEndRegion>();
+}
+
+[Serializable]
+public class MapEndRegion
+{
+    public Vector2 center;
+    public Vector2 bounds;
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "Scriptable Objects/Wave")]
+public class Wave : ScriptableObject
+{
+    public List<WaveComponent> backLineComponents = new List<WaveComponent>();
+    public List<WaveComponent> frontLineComponents = new List<WaveComponent>();
+}
+
+[System.Serializable]
+public class WaveComponent
+{
+    public GameObject alien;
+    public int count;
+    public float spawnDelay;
+    public float afterSpawnDelay;
+}
